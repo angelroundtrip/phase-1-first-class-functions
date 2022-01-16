c63c5c46df1ed945cfe3f299b708ed91628ccf34
Angel Rosario <arosario.us@gmail.com>
2022-01-16 17:44:18 -0500

    commit tree caecc0d014dff930469fcfaba4fca11665e0d14e

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+The receivesAFunction function should:
+take a callback function as an argument
+call the callback function
+it doesn't matter what this function returns, so long as it calls the callback function
+*/
+function receivesAFunction() {
+
+}
+
+/*
+The returnsANamedFunction function should:
+take no arguments
+return a named function
+*/
+function returnsANamedFunction() {
+    
+}
+
+/*
+The returnsAnAnonymousFunction function should:
+take no arguments
+return an anonymous function
+*/
+function returnsAnAnonymousFunction() {
+    
+}
